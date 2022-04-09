34ef34ef917fa4eb810287aa92ea4f87efa956e1
jchanpark <junpark4936@gamil.com>
2022-04-09 13:09:56 -0400

    table schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,26 @@
--- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS timeslots CASCADE;
+DROP TABLE IF EXISTS events CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  availability BOOLEAN,
+  cookie VARCHAR(255)
+);
+
+CREATE TABLE timeslots (
+  id SERIAL PRIMARY KEY NOT NULL,
+  time TIMESTAMP,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY NOT NULL,
+  description TEXT,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  timeslot_id INTEGER REFERENCES timeslots(id) ON DELETE CASCADE,
+  url VARCHAR(255)
 );

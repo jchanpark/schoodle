907bd4c71b1ee0aa197becdc00a80aa1da64290e
Rabhas <rabhas.vashisht@gmail.com>
2022-04-11 02:13:11 +0000

    Add calendar section with date picker and time selector

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,51 @@
 // Client facing scripts here
+$(document).ready(function() {
+$(function(){
+  $("#date").datepicker();
+  // $("#date").datepicker("show");
+})
+
+    // $( "#anim" ).on( "change", function() {
+    //   $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

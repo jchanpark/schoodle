fd5f6bc9922a55ec5a8d74cb8daabccc701a06d3
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-04-12 03:01:38 +0000

    Squashed commit of DB

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,33 @@
--- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS timeslots CASCADE;
+DROP TABLE IF EXISTS events CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  cookie VARCHAR(255)
+);
+
+CREATE TABLE timeslots (
+  id SERIAL PRIMARY KEY NOT NULL,
+  event_id INTEGER REFERENCES events(id) ON DELETE CASCADE,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
+);
+
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  organizer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  url VARCHAR(255)
+);
+
+CREATE TABLE attendances (
+  id SERIAL PRIMARY KEY NOT NULL,
+  timeslot_id INTEGER REFERENCES timeslots(id) ON DELETE CASCADE,
+  attendee_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  attend BOOLEAN
 );

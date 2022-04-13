01ae1c132e1e533ceeaf791a40c66fd057c39063
Rabhas <rabhas.vashisht@gmail.com>
2022-04-13 02:54:25 +0000

    creat an event date table for dates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,6 +33,20 @@ $("#continue-btn").on("click", function () {
 $(".add-btn").on("click", function () {
   event_date_count++;
 
+  var myTab = document.getElementById('date-entry');
+
+  // LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
+  for (i = 1; i < myTab.rows.length; i++) {
+
+      // GET THE CELLS COLLECTION OF THE CURRENT ROW.
+      var objCells = myTab.rows.item(i).cells;
+
+      // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
+      for (var j = 0; j < objCells.length; j++) {
+          alert(objCells.item(j).innerHTML);
+      }
+  }
+
   var startDate = Date.parse($('#start-time').val());
   var endDate = Date.parse($('#end-time').val());
   if (startDate >= endDate) { alert("Please enter proper date") }
@@ -41,7 +55,7 @@ $(".add-btn").on("click", function () {
     alert("You've reached the maximum number of event timeslots.");
   } else {
     const $entry = createDateEntry();
-    $(".date-entry").append($entry)
+    $("#date-entry").append($entry)
     $(".date").datepicker({
       minDate: '+1D'
     });
@@ -90,10 +104,16 @@ $(".nav-create-event").on("click", function () {})
 
 
 const createDateEntry = function() {
-  const entry = $(`<p>Date: <input type="text" class="date" >
-  Start Time: <input type="time" id="start-time" min="05:00" max="24:00" required>
-  End Time: <input type="time" id="end-time" min="05:00" max="24:00" required>
-  <button class="minus-btn" type="submit"> <i class="fa-solid fa-minus"> </i></button> </p>`);
+  // const entry = $(`<p>Date: <input type="text" class="date" >
+  // Start Time: <input type="time" id="start-time" min="05:00" max="24:00" required>
+  // End Time: <input type="time" id="end-time" min="05:00" max="24:00" required>
+  // <button class="minus-btn" type="submit"> <i class="fa-solid fa-minus"> </i></button> </p>`);
+  const entry = $(`<tr>
+  <td><input type="text" class="date" ></td>
+      <td><input type="time" id="start-time" required></td>
+          <td><input type="time" id="end-time"  required></td>
+              <td><button class="minus-btn" type="button" ><i class="fa-solid fa-minus"> </i></button></td>
+</tr>`);
   return entry;
 };
 

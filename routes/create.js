385b4fc64e541667499364be1f33b30cba0c8c32
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-04-13 22:25:06 +0000

    update seeds, fix routes to properly create new event, update test commands

--- routes/create.js
+++ routes/create.js
@@ -23,44 +23,108 @@ const createRouter = db => {
     return res.render("index", templateVars);
   });
 
+  /* Function to authenticate user, getting and/or setting cookie
+     to save user email, and adding user to database if not already in db.
+     This function returns an integer user_id. */
+  const authUser = function(req) {
+    return new Promise((resolve, reject) => {
+
+      // Get or set cookie to authenticate event
+      // let user_email = req.session.user_id;
+      // if (!user_email) {
+      //   user_email = req.body.email;
+      //   req.session.user_id = user_email;
+      // }
+      let user_email = req.body.email;
+
+      // Check if user in db; if not, create user
+      let userExists = false;
+      // let user_id = 0;
+      const queryUserExists = `
+      SELECT * FROM users WHERE email = $1
+      ;`;
+
+      db.query(queryUserExists, [user_email])
+        .then(result => {
+          // console.log("First query result:", result);
+          if (result.rows.length) {
+            userExists = true;
+          }
+          if (!userExists) {
+            const queryUser = `
+            INSERT INTO users (name, email)
+            VALUES ($1, $2)
+            RETURNING *
+            ;`;
+            return db.query(queryUser, [req.body.name, req.body.email])
+              .then(result2 => {
+                console.log("User inserted:", result2.rows);
+                return resolve(result2.rows[0].id);
+              });
+          }
+          console.log("Users found:", result.rows)
+          return resolve(result.rows[0].id);
+        })
+        // .then(result => {
+        //   // if (userExists) {
+        //   //   console.log("User found in database:", result.rows);
+        //   //   return result.rowsid;
+        //   // }
+        //   console.log("Result of query:", result.rows[0].id);
+        //   return result.rows[0].id;
+        // })
+        .catch(err => {
+          console.log("Error in validating user:", err.message);
+        })
+
+    });
+  };
 
   /* POST request for /create/
      Creating a new event
      index posts here */
   router.post('/', (req, res) => {
 
-    // Get or set cookie to authenticate event
-    let user_id = req.session.user_id;
-    if (!user_id) {
-      user_id = req.body.email;
-      req.session.user_id = user_id;
-    }
+    // const auth = new Promise((res, rej) => {
+    //   const user_id = authUser(req);
+    //   res(user_id);
+    // });
+    // Get or create user and get user_id
+    // const user_id = authUser(req);
+    // console.log("Auth user:",authUser(req));
 
-    // Generate random string as unique id/url
-    const url = generateRandomString(30);
-
-    // Insert new event into events table
-    const query = `
-    INSERT INTO events (title, description, user_id, timeslots_id, url)
-    VALUES (
-      $1, $2, $3, $4, ${url}
-    ) RETURNING *; `;
-    const queryParams = [
-      req.body.title,
-      req.body.description,
-      req.body.user_id,
-      req.body.timeslots_id
-    ];
-    // TODO: to populate with request props
-    console.log("Query:", query, queryParams);
-
-    db.query(query, queryParams)
+    authUser(req)
+      // .then(result => {
+      //   console.log("result of auth user", result);
+      // })
       .then(result => {
-        console.log('Event Created:', result.rows);
-        return res.redirect(`/event/${url}`);
+        const user_id = result;
+
+        // Generate random string as unique id/url
+        const url = generateRandomString(30);
+
+        // Insert new event into events table
+        const query = `
+        INSERT INTO events (title, description, organizer_id, url)
+        VALUES (
+          $1, $2, $3, $4
+        ) RETURNING *; `;
+        const queryParams = [
+          req.body.title,
+          req.body.description,
+          user_id,
+          url
+        ];
+        console.log("Query:", query, queryParams);
+
+        return db.query(query, queryParams)
+          .then(result => {
+            console.log('Event Created:', result.rows);
+            return res.redirect(`/event/${url}`);
+          })
       })
       .catch(err => {
-        console.log(`Error in updating event:`, err.message);
+        console.log(`Error in creating event:`, err);
         res.redirect('/?eventErr=true'); // go back to index, with event error
       });
 
